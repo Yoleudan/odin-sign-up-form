4b8f462a0a554570a322ea5ab7b4bcb92bdceb6f
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-04-01 15:10:47 +0100

    Add outline to password boxes if they do not match

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const error = document.querySelector('#error');
+const pwconfirm1 = document.getElementById('confirmPassword');
+const pw1 = document.getElementById('password')
 
 function confirmPassword(){
     const pw = document.getElementById('password').value
@@ -7,31 +9,36 @@ function confirmPassword(){
         const submitBtn = document.getElementById('submitBtn').disabled = true
         document.getElementById('confirmPassword').setCustomValidity("Invalid field.");
         error.textContent="Passwords do not match!"
+        pw1.style.border = "2px solid red"
+        pwconfirm1.style.border = "2px solid red"
         console.log("button disabled")
     }
     else{
-        error.textContent=""
+        error.textContent="match!"
+        error.style.color= "#05f551"
+        pw1.style.border="2px solid #05f551"
+        pwconfirm1.style.border="2px solid #05f551 "
     }
 
 }
 
-const pwconfirm = document.getElementById('confirmPassword');
 
-pwconfirm.addEventListener('focusout', function(){
+
+pwconfirm1.addEventListener('focusout', function(){
     confirmPassword();
 })
 
-const pw = document.getElementById('password')
-pw.addEventListener('focusout', function(){
+
+pw1.addEventListener('focusout', function(){
     confirmPassword();
 })
 
-pwconfirm.addEventListener('focusin', function(){
+pwconfirm1.addEventListener('focusin', function(){
     document.getElementById('submitBtn').disabled = false
     document.getElementById('confirmPassword').setCustomValidity("")
 
 })
-pw.addEventListener('focusin', function(){
+pw1.addEventListener('focusin', function(){
     document.getElementById('submitBtn').disabled = false
     document.getElementById('confirmPassword').setCustomValidity("")
 
